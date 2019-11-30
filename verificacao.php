25e1f217e681883db54a23a3e6845f471af8c9e3
diogo20eng <57547703+diogo20eng@users.noreply.github.com>
2019-11-30 18:31:47 +0000

    Add files via upload

---
+++ verificacao.php
@@ -0,0 +1,10 @@
+<?php
+   echo "Logged In successfully";
+  
+   
+    
+
+?>
+   <script type="text/javascript">
+        setTimeout(function() { location.href = 'index.html'; }, 300);
+    </script>'
